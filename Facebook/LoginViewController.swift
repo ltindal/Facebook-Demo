1d639793d2a40014a9729851944db6a3995f94cf
Lauren <ltindal19@gmail.com>
2016-10-20 20:57:35 -0700

    Added Login Logic

--- Facebook/LoginViewController.swift
+++ Facebook/LoginViewController.swift
@@ -12,10 +12,13 @@ class LoginViewController: UIViewController {
 
     
     @IBOutlet weak var logo: UIImageView!
-    
     @IBOutlet weak var fieldSuperView: UIView!
-    
     @IBOutlet weak var labelSuperview: UIView!
+    
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     var logoInitialY: CGFloat!
     var fieldInitialY: CGFloat!
@@ -51,6 +54,36 @@ class LoginViewController: UIViewController {
 
     @IBAction func didTapOutside(_ sender: AnyObject) {
         view.endEditing(true)
+    }
+    
+    
+    @IBAction func didPressLogin(_ sender: AnyObject) {
+        activityIndicator.startAnimating()
+        
+        if emailField.text == "ltindal@box.com" &&
+            passwordField.text == "password" {
+            
+            loginButton.isSelected = true
+            
+            delay(2, closure: { 
+               activityIndicator.stopAnimating()
+                
+               
+                
+            })
+        }
+        else {
+            activityIndicator.stopAnimating()
+            loginButton.isSelected = true
+            
+            delay(2, closure: {
+                let alertController = UIAlertController(title: "Try Again", message: "Your email or password is incorrect.", preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "Ok", style: .default, handler: { (<#UIAlertAction#>) in
+                })
+                alertController.addAction(okAction)
+                present(alertController, animated: true)
+            })
+        }
     }
 
 
